b1d613ee039083ee39155e7aaecb4b3637d25930
DAI <malias@mitaoe.ac.in>
2024-12-28 12:01:53 +0530

    hello

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'Production ready'
+	return 'The Jenkins CICD pipeline is working!!'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
